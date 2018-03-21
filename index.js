04c2b0b6e6d947cf794e4d5ae51b4dd83d7155bb
Sameer Kanakia <skanakia1@gmail.com>
2018-03-21 15:03:41 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+  var number = a + b
+  return number
+} 
+
+function subtract(a, b) {
+  var number = a - b
+  return number
+}
+
+function multiply(a, b) {
+  var number = a * b
+  return number
+}
+
+function divide(a, b) {
+  var number = a / b
+  return number
+}
